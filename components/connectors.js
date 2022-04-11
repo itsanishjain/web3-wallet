9d8ab30c6722b00e0cc0dde1d2f0b6e8cc9df79d
anish jain <helloanishjain@gmail.com>
2022-04-11 17:39:27 +0530

    remove rpc

--- components/connectors.js
+++ components/connectors.js
@@ -5,8 +5,19 @@ const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
+// const INFURA_NETWORK_URLS = {
+//   1 : `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+//   2:
+// }
+
+// const ALL_SUPPORTED_CHAIN_IDS = {
+
+// }
+
 const walletconnect = new WalletConnectConnector({
-  rpcUrl: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
+  // rpcUrl: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
+  // supportedChainIds: ALL_SUPPORTED_CHAIN_IDS,
+  // rpc: INFURA_NETWORK_URLS,
   bridge: "https://bridge.walletconnect.org",
   qrcode: true
 });
