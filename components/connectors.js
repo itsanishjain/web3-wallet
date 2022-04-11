a07871b19a20ed042de8c7efa84516869bc78889
anish jain <helloanishjain@gmail.com>
2022-04-11 17:50:26 +0530

    added rpc and support chain id

--- components/connectors.js
+++ components/connectors.js
@@ -5,19 +5,25 @@ const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42]
 });
 
-// const INFURA_NETWORK_URLS = {
-//   1 : `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
-//   2:
-// }
+
+const infuraId = "dc2e719674974f18912598de53fb7e74"
+
+const ALL_SUPPORTED_CHAIN_IDS = [1, 2, 4]
+
+const INFURA_NETWORK_URLS = {
+  1: `https://mainnet.infura.io/v3/${infuraId}`,
+  2: `https://ropsten.infura.io/v3/${infuraId}`,
+  4: `https://rinkeby.infura.io/v3/${infuraId}`
+}
 
 // const ALL_SUPPORTED_CHAIN_IDS = {
 
 // }
 
 const walletconnect = new WalletConnectConnector({
-  // rpcUrl: `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
-  // supportedChainIds: ALL_SUPPORTED_CHAIN_IDS,
-  // rpc: INFURA_NETWORK_URLS,
+  // rpcUrl: `https://rinkeby.infura.io/v3/${infuraId}`,
+  supportedChainIds: ALL_SUPPORTED_CHAIN_IDS,
+  rpc: INFURA_NETWORK_URLS,
   bridge: "https://bridge.walletconnect.org",
   qrcode: true
 });
