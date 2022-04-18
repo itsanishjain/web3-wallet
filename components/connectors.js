99de8b84c8ad0b3a8768b514573cb2227e4d7745
anish jain <helloanishjain@gmail.com>
2022-04-18 13:58:07 +0530

    remove unused chaiId

--- components/connectors.js
+++ components/connectors.js
@@ -2,11 +2,11 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+  supportedChainIds: [1, 3, 4]
 });
 
 
-const ALL_SUPPORTED_CHAIN_IDS = [1, 2, 4]
+const ALL_SUPPORTED_CHAIN_IDS = [1, 3, 4]
 
 const INFURA_NETWORK_URLS = {
   1: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`,
