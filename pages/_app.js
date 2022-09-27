b3755b828331107de44bff168b3aa829e98fa9b3
Anish <helloanishjain@gmail.com>
2022-09-27 12:40:40 +0530

    add web3 modal for connect and disconnect

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { chains, providers } from "@web3modal/ethereum";
+import { Web3ModalProvider } from "@web3modal/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Get projectID at https://cloud.walletconnect.com
+if (!process.env.NEXT_PUBLIC_PROJECT_ID)
+  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable");
+
+// Configure web3modal
+const modalConfig = {
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  theme: "dark",
+  accentColor: "default",
+  ethereum: {
+    appName: "web3Modal",
+    autoConnect: true,
+    chains: [chains.polygonMumbai],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+      }),
+    ],
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Web3ModalProvider config={modalConfig}>
+      <Component {...pageProps} />
+    </Web3ModalProvider>
+  );
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import Home from '../components/Home';
-import { ethers } from 'ethers';
-import { Web3ReactProvider } from '@web3-react/core';
-
-const getLibrary = (provider) => {
-  return new ethers.providers.Web3Provider(provider);
-}
+import { ConnectButton, useAccount } from "@web3modal/react";
+import { ClientCtrl } from "@web3modal/core";
 
 export default function App() {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Home />
-    </Web3ReactProvider>
-  )
+  const { connected } = useAccount();
+
+  return connected ? (
+    <>
+      <button onClick={ClientCtrl.ethereum().disconnect}>Disconnect</button>
+      <div>
+        <buttom>Get Data</buttom>
+      </div>
+    </>
+  ) : (
+    <ConnectButton />
+  );
 }
